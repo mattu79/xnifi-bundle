f26020d1852e33825a125b76b6f61e159cf4b9bd
yuxinhui <yuxinhui@huiwan.com>
2019-12-26 15:39:06 +0800

    增加EnrichRecord处理器

--- xnifi-processors/src/main/java/io/activedata/xnifi2/processors/enrich/EnrichRecord.java
+++ xnifi-processors/src/main/java/io/activedata/xnifi2/processors/enrich/EnrichRecord.java
@@ -20,11 +20,14 @@ package io.activedata.xnifi2.processors.enrich;
 import io.activedata.xnifi2.core.batch.AbstractBuilderSupportProcessor;
 import io.activedata.xnifi2.core.batch.Input;
 import io.activedata.xnifi2.core.batch.Output;
+import org.apache.commons.lang3.StringUtils;
 import org.apache.nifi.annotation.behavior.*;
 import org.apache.nifi.annotation.behavior.InputRequirement.Requirement;
 import org.apache.nifi.annotation.documentation.CapabilityDescription;
 import org.apache.nifi.annotation.documentation.Tags;
+import org.apache.nifi.components.AllowableValue;
 import org.apache.nifi.components.PropertyDescriptor;
+import org.apache.nifi.expression.ExpressionLanguageScope;
 import org.apache.nifi.flowfile.FlowFile;
 import org.apache.nifi.processor.ProcessContext;
 import org.apache.nifi.processor.Relationship;
@@ -33,10 +36,7 @@ import org.apache.nifi.processor.util.StandardValidators;
 import org.apache.nifi.serialization.record.Record;
 import org.apache.nifi.util.Tuple;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Map;
-import java.util.Optional;
+import java.util.*;
 
 
 @EventDriven
@@ -44,12 +44,18 @@ import java.util.Optional;
 @SupportsBatching
 @InputRequirement(Requirement.INPUT_REQUIRED)
 @WritesAttributes({
-    @WritesAttribute(attribute = "mime.type", description = "数据格式"),
-    @WritesAttribute(attribute = "record.count", description = "FlowFile中记录的数量")
+        @WritesAttribute(attribute = "mime.type", description = "数据格式"),
+        @WritesAttribute(attribute = "record.count", description = "FlowFile中记录的数量")
 })
 @Tags({"lookup", "enrichment", "route", "record", "csv", "json", "avro", "database", "db", "logs", "convert", "filter"})
 @CapabilityDescription("通过检索服务循环查询出对应的结果记录。当检索结果返回时，结果可以可选的附加到原始记录上。")
 public class EnrichRecord extends AbstractBuilderSupportProcessor {
+
+    static final AllowableValue ROUTE_TO_SUCCESS = new AllowableValue("route-to-success", "全部路由到成功队列",
+            "不管是否检索到记录都路由到成功队列");
+
+    static final AllowableValue ROUTE_TO_MATCHED_UNMATCHED = new AllowableValue("route-to-matched-unmatched", "路由到匹配或未匹配队列",
+            "检索到记录时路由到匹配队列，未检索到记录时路由到非匹配队列");
 
     public static final PropertyDescriptor PROP_LOOKUP_SERVICE = new PropertyDescriptor.Builder()
             .name("lookup-service")
@@ -60,12 +66,66 @@ public class EnrichRecord extends AbstractBuilderSupportProcessor {
             .addValidator(StandardValidators.NON_EMPTY_VALIDATOR)
             .build();
 
+    public static final PropertyDescriptor PROP_ROUTING_STRATEGY = new PropertyDescriptor.Builder()
+            .name("routing-strategy")
+            .displayName("路由策略")
+            .description("指定未检索到记录时的路由策略")
+            .expressionLanguageSupported(ExpressionLanguageScope.NONE)
+            .allowableValues(ROUTE_TO_SUCCESS, ROUTE_TO_MATCHED_UNMATCHED)
+            .defaultValue(ROUTE_TO_SUCCESS.getValue())
+            .required(true)
+            .build();
+
+    public Relationship REL_MATCHED = new Relationship.Builder()
+            .name("matched")
+            .description("匹配队列")
+            .build();
+
+    public Relationship REL_UNMATCHED = new Relationship.Builder()
+            .name("unmatched")
+            .description("为匹配队列")
+            .build();
+
+    private Set<Relationship> rels;
+
+    private volatile boolean routeToMatchedUnmatched;
+
     private volatile LookupService lookupService;
+
+    private volatile String routingStrategy;
+
+    @Override
+    public void onPropertyModified(final PropertyDescriptor descriptor, final String oldValue, final String newValue) {
+        if (PROP_ROUTING_STRATEGY.equals(descriptor)) {
+            if (ROUTE_TO_MATCHED_UNMATCHED.getValue().equalsIgnoreCase(newValue)) {
+                final Set<Relationship> matchedUnmatchedRels = new HashSet<>();
+                matchedUnmatchedRels.add(REL_MATCHED);
+                matchedUnmatchedRels.add(REL_UNMATCHED);
+                matchedUnmatchedRels.add(REL_FAILURE);
+                this.rels = matchedUnmatchedRels;
+
+                this.routeToMatchedUnmatched = true;
+            } else {
+                final Set<Relationship> successRels = new HashSet<>();
+                successRels.add(REL_SUCCESS);
+                successRels.add(REL_FAILURE);
+                this.rels = successRels;
+
+                this.routeToMatchedUnmatched = false;
+            }
+        }
+    }
+
+    @Override
+    public Set<Relationship> getRelationships() {
+        return super.getRelationships();
+    }
 
     @Override
     protected List<PropertyDescriptor> getSupportedPropertyDescriptors() {
         List<PropertyDescriptor> props = new ArrayList<PropertyDescriptor>();
         props.add(PROP_LOOKUP_SERVICE);
+        props.add(PROP_ROUTING_STRATEGY);
         props.add(PROP_OUTPUT_RECORD_STRATEGY);
         props.add(PROP_INPUT_RECORD_TYPE);
         props.add(PROP_OUTPUT_RECORD_TYPE);
@@ -78,7 +138,10 @@ public class EnrichRecord extends AbstractBuilderSupportProcessor {
     @Override
     protected void beforeProcess(ProcessContext context) throws ProcessException {
         super.beforeProcess(context);
+
         lookupService = context.getProperty(PROP_LOOKUP_SERVICE).asControllerService(LookupService.class);
+        routingStrategy = context.getProperty(PROP_ROUTING_STRATEGY).getValue();
+        this.routeToMatchedUnmatched = StringUtils.equals(routingStrategy, ROUTE_TO_MATCHED_UNMATCHED.getValue());
     }
 
     @Override
@@ -90,13 +153,22 @@ public class EnrichRecord extends AbstractBuilderSupportProcessor {
     protected Tuple<Relationship, Output> processRecordInternal(Map<String, String> attributes, Input input, FlowFile original, ProcessContext context) throws Exception {
         Optional<Record> optionalRecord = lookupService.lookup(input, attributes);
         Output output = new Output();
-        if (optionalRecord.isPresent()){
+        if (optionalRecord.isPresent()) {
             Record record = optionalRecord.get();
             output.putAll(input);
             output.putAll(record.toMap());
-            return new Tuple<>(REL_SUCCESS, output);
-        }else {
-            return new Tuple<>(REL_FAILURE, output);
+
+            if (routeToMatchedUnmatched) {
+                return new Tuple<>(REL_MATCHED, output);
+            } else {
+                return new Tuple<>(REL_SUCCESS, output);
+            }
+        } else {
+            if (routeToMatchedUnmatched) {
+                return new Tuple<>(REL_UNMATCHED, output);
+            } else {
+                return new Tuple<>(REL_SUCCESS, output);
+            }
         }
     }
 }
